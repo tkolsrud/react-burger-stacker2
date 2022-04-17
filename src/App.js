26b97dc0b56f01ebd7fa8aeebcd688fca5542d32
tkolsrud <tom.kolsrud@generalassemb.ly>
2022-04-16 18:08:52 -0700

    refactored app component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
 import './App.css';
-import BurgerPane from './components/burgerPane'
-import Stack from './components/stack'
-// import Ingredient from './components/ingredient'
-// import ClearStack from './components/clearStack'
+import BurgerStack from './components/burgerStack'
+
 
 function App() {
-  const ingredients = [
-    {name: 'Kaiser Bun', color: 'saddlebrown'},
-    {name: 'Sesame Bun', color: 'sandybrown'},
-    {name: 'Gluten Free Bun', color: 'peru'},
-    {name: 'Lettuce Wrap', color: 'olivedrab'},
-    {name: 'Beef Patty', color: '#3F250B'},
-    {name: 'Soy Patty', color: '#3F250B'},
-    {name: 'Black Bean Patty', color: '#3F250B'},
-    {name: 'Chicken Patty', color: 'burlywood'},
-    {name: 'Lettuce', color: 'lawngreen'},
-    {name: 'Tomato', color: 'tomato'},
-    {name: 'Bacon', color: 'maroon'},
-    {name: 'Onion', color: 'lightyellow'}
-  ]
-
-  return (
-    <div className="App">
-      <h1>Burger Stacker App</h1>
-      <BurgerPane ingredients={ingredients}/>
-      <Stack />
-    </div>
-  );
+  return(
+    <BurgerStack />
+  )
 }
 
 export default App;
